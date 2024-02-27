ddb16349c3909fa142e76e14b7e052986964946b
Eduardo Maldonado <eduardo.maldonado@cfe.mx>
2024-02-27 14:43:57 -0600

    Initial Commit

---
+++ src/main/java/mx/cfe/Main.java
@@ -0,0 +1,10 @@
+package mx.cfe;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Operar operaciones = new Operar();
+        System.out.print(operaciones.sumar(45.0, 3.0));
+    }
+}
